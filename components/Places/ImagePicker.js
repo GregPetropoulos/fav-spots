7f5621acfaa6e612d286c9db43551b34a5bb4b35
GP <gregpetropoulos@yahoo.com>
2024-03-13 13:25:07 -0400

    Implemented the logic for the image picker and associated promises, a preview of an image showing up on ios and adroid platforms

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,11 +1,15 @@
-import { Alert, Button, View } from 'react-native';
-// import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { Alert, Button, View, Image, Text, StyleSheet } from 'react-native';
+import { Colors } from '../../constants/colors';
+
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus
 } from 'expo-image-picker';
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions(); //Need this because ios permissions, android automatically asks for permissions
 
@@ -37,15 +41,36 @@ const ImagePicker = () => {
       aspect: [16, 9],
       quality: 0.5
     });
-    console.log(image);
+    setPickedImage(image.assets[0].uri);
   };
+  let imagePreview = <Text>No Image Taken yet</Text>;
+  if (pickedImage) {
+    imagePreview = (
+      <Image
+        source={{ uri: pickedImage }}
+        style={styles.image}
 
+      />
+    );
+  }
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title='Take Image' onPress={takeImageHandler} />
     </View>
   );
 };
 
 export default ImagePicker;
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4
+  },
+  image:{ width:'100%', height:'100%'}
+});

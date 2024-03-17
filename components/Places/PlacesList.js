a2e1be004d53e99d4dd235f582404a844f726244
GP <gregpetropoulos@yahoo.com>
2024-03-17 14:29:53 -0400

    List items and styling of the image and  info content

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -13,6 +13,7 @@ function PlacesList({ places }) {
 
   return (
     <FlatList
+    style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -21,6 +22,7 @@ function PlacesList({ places }) {
 }
 export default PlacesList;
 const styles = StyleSheet.create({
+  list:{margin:24},
   fallBackContainer: {
     flex: 1,
     justifyContent: 'center',
